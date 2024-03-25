fe48e19439bb9a2a520d769fe5636378dd2605c3
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2024-03-25 13:13:40 +0600

    updated

--- src/main/resources/static/js/form.js
+++ src/main/resources/static/js/form.js
@@ -12,3 +12,13 @@ checkboxes.forEach(checkbox => {
         });
     });
 });
+
+
+const form = document.getElementById('checkboxForm');
+form.addEventListener('submit', function(event) {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    if (checkboxes.length === 0) {
+        alert('Choose any one option');
+        event.preventDefault(); // Prevent form submission
+    }
+});
